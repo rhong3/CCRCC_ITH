fdf6ed7a89107896f67da740796e15369f6f3863
Runyu Hong <michaelhong1995@gmail.com>
2021-04-20 14:53:03 -0400

    4.20.3

--- scripts/Label_prep.py
+++ scripts/Label_prep.py
@@ -1,8 +1,8 @@
 # Prepare labels for CCRCC ITH imaging project
 import pandas as pd
 
-all = pd.read_csv("../cohort.csv")
-all = all.loc[(all["Specimen_Type"] == "tumor_tissue")]
+all = pd.read_csv("../cohort_DF.csv")
+all = all.loc[(all["Specimen_Type"] == "tumor_tissue") & (all["Tumor"] == "CCRCC")]
 samples = pd.read_csv("../CPTAC_ccRCC_ITH_meta_table_v1.0.tsv", sep="\t")
 
 all = all[["Patient_ID", "Slide_ID"]]
@@ -11,5 +11,5 @@ samples = samples[["CASE_ID", "Slide_ID", "Immune_subtype_ITH_cohort", "Source"]
 valid = pd.merge(all, samples, on=["Slide_ID"], how="inner")
 valid['label'] = valid["Immune_subtype_ITH_cohort"].str.split("im", n=1, expand=True)[1]
 valid = valid[["Patient_ID", "Slide_ID", "label", "Source"]]
-
+valid['Slide_ID'] = valid['Slide_ID'].str.split("-", expand=True)[2]
 valid.to_csv("../immune_label.csv", index=False, header=True)
